e78eaf7feb01ecd31a69a154f98af8ae282aecc2
Leo Ware <leoware@uni.minerva.edu>
2021-12-13 17:05:01 +0100

    some of this kind of works

--- src/water.py
+++ src/water.py
@@ -1,72 +1,94 @@
 import numpy as np
 
 
+def sum_neighbor_grid(neighbor_grid):
+    pad_width = [(1, 1), (1, 1), (0, 0), (0, 0)]
+    padded_vmd = np.pad(neighbor_grid, pad_width, constant_values=0)
+
+    acc = np.full(padded_vmd.shape[:2], 0)
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            offset = np.roll(padded_vmd, (i, j), (0, 1))[:, :, i + 1, j + 1]
+            acc = offset + acc
+
+    return acc[1:-1, 1:-1]
+
+
+def get_neighbors(grid):
+    neighbors = np.zeros(grid.shape + (3, 3))
+    for i in [0, 1, 2]:
+        for j in [0, 1, 2]:
+            neighbors[:, :, i, j] = np.roll(grid, (1 - i, 1 - j), axis=(0, 1))
+    return neighbors[1:-1, 1:-1, :, :]
+
+
 class Water:
-    def __init__(self, dem, source=None, manning=0.02):
+    def __init__(self, dem, source=None):
         self.dem = np.array(dem, dtype=float)
+        self.source = source if source else np.zeros_like(self.dem, dtype=float)
+        self.depth = np.zeros_like(self.dem, dtype=float)
 
         if len(self.dem.shape) != 2 or not all(self.dem.shape):
             raise ValueError("dem must be 2-dimensional")
 
-        self.source = source if source else np.zeros_like(self.dem)
-        self.manning = manning if type(manning) == np.ndarray else np.full_like(self.dem, manning)
-
-        self.depth = np.zeros_like(self.dem)
-        self.slope = self.calc_slope()
-        self.direction = self.calc_flow_direction()
-        self.volume_moving = self.calc_volume_moving()
-
-    def calc_flow_direction(self):
-        # extend the dem with a border that is level with it
+    def calc_height(self):
         padded_dem = np.pad(self.dem, 1, mode="edge")
+        padded_depth = np.pad(self.depth, 1, constant_values=0)
+        padded_height = padded_depth + padded_dem
+        return get_neighbors(padded_height)
 
-        neighbors = np.zeros(padded_dem.shape + (3, 3))
-        for i in [0, 1, 2]:
-            for j in [0, 1, 2]:
-                neighbors[:, :, i, j] = np.roll(padded_dem, (1 - i, 1 - j), axis=(0, 1))
-        neighbors[:, :, 1, 1] = np.inf
+    def calc_outflow(self, height):
+        lower = height <= height[:, :, 1, 1].reshape(height.shape[:2] + (1, 1))
 
-        mins = np.min(neighbors, axis=(2, 3))
-        direction = mins.reshape(mins.shape + (1, 1)) == neighbors
-        n_directions = np.sum(direction, axis=(2, 3)).reshape(direction.shape[:2] + (1, 1))
-        direction = direction/n_directions
-        return direction[1:-1, 1:-1]
+        # find the average height among lower cells
+        n_lower = np.sum(lower, axis=(2, 3))
+        lower_sum = np.sum(np.where(lower, height, 0), axis=(2, 3))
+        lower_avg = lower_sum / n_lower
 
-    def calc_slope(self):
-        dx, dy = np.gradient(self.dem)
-        return (dx**2 + dy**2)**0.5
+        # remove water to bring lower neighboring cells up to the average height
+        return np.minimum(self.dem + self.depth - lower_avg, self.depth)
 
-    def calc_volume_moving(self):
-        # dimensions??
-        water_speed = self.depth**(3/2)*self.slope**(1/2)/self.manning
-        flooded = water_speed > 1
-        volume_moving = water_speed*self.depth
-        volume_moving[~flooded] = 0
-        return volume_moving
+    def calc_inflow(self, outflow, height):
+        height = height.copy()
+        height[:, :, 1, 1] -= outflow
+        height = height.reshape(self.dem.shape + (-1,))
 
-    def calc_inflow(self, volume_moving):
-        volume_moving_directional = self.direction * volume_moving.reshape(volume_moving.shape + (1, 1))
-        npad = [(1, 1), (1, 1), (0, 0), (0, 0)]
-        padded_vmd = np.pad(volume_moving_directional, npad, constant_values=0)
+        sort_indices = np.argsort(height)
+        sorted_heights = np.take_along_axis(height, sort_indices, -1)
 
-        acc = np.zeros(padded_vmd.shape[:2])
-        for i in [-1, 0, 1]:
-            for j in [-1, 0, 1]:
-                acc += np.roll(padded_vmd, (i, j), (0, 1))[:, :, i + 1, j + 1]
+        # whole-level water allocations
+        volume_needed_to_fill = np.cumsum(
+            np.diff(sorted_heights, axis=-1) *
+            np.arange(1, 9).reshape((1, 1, -1)),
+            axis=-1)
+        index_last_filled = np.sum(volume_needed_to_fill <= outflow.reshape(3, 3, 1), axis=-1).reshape([3, 3, 1])
+        fill_to_depth = np.take_along_axis(sorted_heights, index_last_filled, -1)
+        relative_heights = sorted_heights - np.min(sorted_heights, axis=-1).reshape([3, 3, 1])
+        integer_allocations = fill_to_depth - relative_heights
 
-        return acc[1:-1, 1:-1]
+        # fractional-level water allocations
+        used_water = np.take_along_axis(volume_needed_to_fill, index_last_filled - 1, -1)
+        used_water[index_last_filled == 0] = 0
+        remainder = outflow - used_water.reshape([3, 3])
+        fractional_allocations = remainder.reshape(3, 3, 1) / index_last_filled
 
-    def calc_inundation(self):
-        return np.zeros_like(self.dem)
+        # combine and zero out non-allocating spaces
+        allocations = fractional_allocations + integer_allocations
+        nonallocating = np.arange(9).reshape([1, 1, 9]) > index_last_filled
+        allocations[nonallocating] = 0
+
+        # rearrange results
+        inverse_sort_indices = np.take_along_axis(np.arange(9).reshape([1, 1, 9]), sort_indices, -1)
+        unsorted_allocations = np.take_along_axis(allocations, inverse_sort_indices, -1)
+        final_allocations = sum_neighbor_grid(unsorted_allocations.reshape(self.dem.shape + (3, 3)))
+
+        return final_allocations
 
     def calc_update(self):
-        volume_moving = self.calc_volume_moving()
-
-        outflow = -volume_moving
-        inflow = self.calc_inflow(volume_moving)
-        inundation = self.calc_inundation()
-
-        return self.source + outflow + inflow + inundation
+        height = self.calc_height()
+        outflow = self.calc_outflow(height)
+        inflow = self.calc_inflow(outflow, height)
+        return self.source + inflow
 
     def step(self):
-        self.depth += self.calc_update()
+        self.depth = self.calc_update()
--- tests/test_water.py
+++ tests/test_water.py
@@ -1,17 +1,15 @@
 import numpy as np
 from pytest import raises
-from src.water import Water
+from src.water import Water, balance_levels, sum_neighbor_grid, calc_at_neighbors
 
 
 def test_init():
-    arr = np.array([
+    arr = [
         [1, 2],
         [3, 4]
-    ])
+    ]
     foo = Water(arr)
-
-    assert foo.dem.tolist() == arr.tolist()
-    assert np.all(foo.slope > 0)
+    assert foo.dem.tolist() == arr
 
 
 def test_errs():
@@ -22,35 +20,71 @@ def test_errs():
         Water(np.array([[]]))
 
 
-def test_flow_direction():
-    foo = Water([
+def test_balance_levels():
+    assert balance_levels(np.array([2, 1, 3]), 0).tolist() == [0, 0, 0]
+    assert balance_levels(np.array([2, 1, 3]), 0.1).tolist() == [0, 0.1, 0]
+    assert balance_levels(np.array([2, 1, 3]), 2).tolist() == [0.5, 1.5, 0]
+    assert balance_levels(np.array([2, 1, 3]), 3).tolist() == [1, 2, 0]
+    assert balance_levels(np.array([2, 1, 3]), 5).tolist() == [1 + 2/3, 2 + 2/3, 2/3]
+
+
+def test_calc_at_neighbors():
+    foo = calc_at_neighbors(np.array([
         [1, 2],
         [3, 4]
+    ]))
+
+    assert foo[0, 0].tolist() == [
+        [1, 1, 2],
+        [1, 1, 2],
+        [3, 3, 4]
+    ]
+
+
+def test_sum_neighbor_grid():
+    grid = np.array([
+        [1, 2],
+        [3, 4],
+    ])
+    assert sum_neighbor_grid(calc_at_neighbors(grid)).tolist() == (4*grid).tolist()
+
+
+def test_calc_outflow():
+    foo = Water(np.zeros((3, 3)))
+    foo.depth = np.array([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ])
+    assert foo.calc_outflow().tolist() == [
+        [0, 0, 0],
+        [0, 8/9, 0],
+        [0, 0, 0]
+    ]
+
+    foo.depth = np.array([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ])
+    assert foo.calc_outflow().tolist() == [
+        [0.0, 0.0, 0.0],
+        [0.0, 7/9, 0.0],
+        [0.0, 0.6666666666666667, 0.0]
+    ]
+
+
+def test_calc_inflow():
+    foo = Water([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
     ])
 
-    assert foo.direction[0, 0].tolist() == [
-        [1/3, 1/3, 0],
-        [1/3, 0, 0],
-        [0, 0, 0]
-    ]
+    outflow = foo.calc_outflow()
+    inflow = foo.calc_inflow(outflow)
 
-    assert foo.direction[1, 0].tolist() == [
-        [0.5, 0.5, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ]
-
-    assert foo.direction[0, 1].tolist() == [
-        [1 / 2, 0, 0],
-        [1 / 2, 0, 0],
-        [0, 0, 0]
-    ]
-
-    assert foo.direction[1, 1].tolist() == [
-        [1, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ]
+    assert False
 
 
 def test_step():
