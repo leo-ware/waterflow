206f42917c7d24989a4fa2d1d1e15e4a88d74970
Leo Ware <leoware@uni.minerva.edu>
2021-12-16 20:33:26 +0100

    rainfall

--- src/water.py
+++ src/water.py
@@ -1,23 +1,22 @@
 from src.functions import *
 import jax
 import jax.numpy as jnp
+import numpy as np
 
 
 class Water:
-    def __init__(self, ground, water=None):
-        self.ground = jnp.array(ground)
-        if water is not None:
-            self.water = jnp.array(water)
-        else:
-            self.water = jnp.zeros_like(water)
+    def __init__(self, ground_level, water_level=None, chance_rain=None):
+        self.ground_level = jnp.array(ground_level)
+        self.water_level = jnp.array(water_level) if water_level is not None else jnp.zeros_like(self.ground_level)
+        self.chance_rain = jnp.array(chance_rain) if chance_rain is not None else jnp.zeros_like(self.ground_level)
 
     # @jax.jit
     def gravity(self):
         """Calculates new water layout based on effects of gravity"""
-        neighbors = get_neighbors(self.ground + self.water)
-        neighbors = neighbors.at[:, :, 1, 1].set(self.ground)
+        neighbors = get_neighbors(self.ground_level + self.water_level)
+        neighbors = neighbors.at[:, :, 1, 1].set(self.ground_level)
         flat, sort_indices = flatten_sort(neighbors)
-        water_flat = self.water.reshape((-1, 1))
+        water_flat = self.water_level.reshape((-1, 1))
 
         stacked = jnp.hstack([water_flat, flat])
         balanced = v_balance(stacked)
@@ -26,5 +25,10 @@ class Water:
         new_water = sum_neighbor_grid(unsorted_balanced)
         return new_water
 
+    def rainfall(self):
+        """Calculates water to add from rainfall"""
+        rain = (np.random.random(self.chance_rain.shape) <= self.chance_rain).astype(int)
+        return rain
+
     def step(self):
-        self.water = self.gravity()
+        self.water_level = self.gravity() + self.rainfall()
--- tests/test_water.py
+++ tests/test_water.py
@@ -5,8 +5,8 @@ from pytest import approx
 
 def test_gravity():
     foo = Water(
-        ground=np.zeros((3, 3)),
-        water=[
+        ground_level=np.zeros((3, 3)),
+        water_level=[
             [0, 0, 0],
             [0, 1, 0],
             [0, 0, 0]
@@ -15,3 +15,17 @@ def test_gravity():
     spread_out = np.ones((3, 3))/9
 
     assert (foo.gravity().flatten()) == approx(list(spread_out.flatten()))
+
+
+def test_rainfall():
+    shape = (100, 100)
+    n_cells = np.prod(shape)
+
+    foo = Water(ground_level=np.zeros(shape), chance_rain=np.zeros(shape))
+    assert np.sum(foo.rainfall()) == 0
+
+    foo = Water(ground_level=np.zeros(shape), chance_rain=np.ones(shape))
+    assert np.sum(foo.rainfall()) == n_cells
+
+    foo = Water(ground_level=np.zeros(shape), chance_rain=np.full(shape, 0.8))
+    assert 0 < np.sum(foo.rainfall()) < n_cells
